478a3147e8acc23696c45e6bea4170dfb8dc8eb1
gildardoalvarado <galvarado@is4tech.com>
2022-08-31 12:35:47 -0600

    Create componenet in general module

--- src/app/portal/modulegeneral/general.module.ts
+++ src/app/portal/modulegeneral/general.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidenavComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
